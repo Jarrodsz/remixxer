e0d685da6951074d11050e2afb725dbb7647b093
jordan <info@acquired.nl>
2022-12-12 20:24:40 +0100

    add example links to /index path

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,6 @@
-import {Link} from "@remix-run/react";
+import {Link, NavLink} from "@remix-run/react";
 import {Box} from "@chakra-ui/react";
+import {NavItem} from "@saas-ui/sidebar";
 
 export default function Index() {
     return (
@@ -8,6 +9,11 @@ export default function Index() {
 
             <Box>
                 <Link to={"/admin"}>Link to admin</Link>
+
+                <NavLink to={"user/support"}>NavLink</NavLink>
+                <Link to={"user/support"}>Link</Link>
+                <NavItem label="xxx" size="lg" href={"user/support"} />
+
             </Box>
         </>
     );
